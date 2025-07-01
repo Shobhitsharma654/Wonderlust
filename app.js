7607c552c162f1154055235c1e7acac801378a1e
Shobhit sharma <shobhitss652@gmail.com>
2025-07-01 12:07:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,11 +86,6 @@ app.use((req, res, next) => {
 });
 
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
-
-
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
